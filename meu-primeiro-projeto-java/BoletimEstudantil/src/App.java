0ec179ecd399688ef4b7673fbe8ebff7f29e4587
DoguetiDoCapao <douglas.amaral2006@gmail.com>
2024-09-29 21:42:08 -0300

    Boletim Estudantil em JAVA

--- meu-primeiro-projeto-java/BoletimEstudantil/src/App.java
+++ meu-primeiro-projeto-java/BoletimEstudantil/src/App.java
@@ -1,5 +1,5 @@
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Boa Boa Fiot!");
+        System.out.println("Hello, World!");
     }
 }
