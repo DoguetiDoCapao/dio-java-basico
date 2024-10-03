8cf4d4f4bb457008ce9e02346247fb3109b58220
DoguetiDoCapao <douglas.amaral2006@gmail.com>
2024-10-03 19:50:44 -0300

    terceiro commit

---
+++ meu-primeiro-projeto-java/BoletimEstudantil/src/edu/douglas/segundasemana/Desculpa100Vezes/src/Desculpa100Vezes.java
@@ -0,0 +1,7 @@
+public class Desculpa100Vezes {
+    public static void main(String[] args) {
+        for (int i = 1; i <= 10; i++) {
+            System.out.println(i + "- Desculpa");
+        }
+    }
+}
